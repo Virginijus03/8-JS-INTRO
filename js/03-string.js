705c79fab13227ab9c466cb116e1fa1f92bfd89b
Virginijus03 <klimauskas@yahoo.com>
2021-05-05 17:40:18 +0300

    Strings

--- js/03-string.js
+++
@@ -1,4 +0,0 @@
-
-const kabutes1 = "Labas";
-const kabutes2 = `vakaras`;
-console.log(kabutes1, kabutes2);
---
+++ js/03-strings.js
@@ -0,0 +1,22 @@
+/*
+
+STRINGS(tekstas)
+
+Iniciavimo(rasymo, kodavimo) budai:
+1)viengubos(')kabutes
+2)dvibubos(¨)kabutes
+3)
+
+*/
+const kabutes1 = "Labas";
+const kabutes2 = `vakaras`;
+console.log(kabutes1, kabutes2);
+
+//Viengubos (') kabutes
+const kabutes21 = "Viengubos (') kabutes";
+console.log(kabutes21);
+
+//Dvigubos (¨) kabutes
+const kabutes12 = 'Dvigubos (¨) kabutes';
+console.log(kabutes12);
+
