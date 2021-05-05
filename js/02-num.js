1826526c82ec85ef42c635353c12e23a3e69fb34
Virginijus03 <klimauskas@yahoo.com>
2021-05-05 17:19:53 +0300

    Numbers

--- js/02-num.js
+++ js/02-num.js
@@ -1,2 +1,30 @@
+/*
+
+NUMBERS
+
+1)teigiami, nulis, neigiami
+2)desimtainiai, sveikieji
+3)tikri skaiciai, begalybes, NaN
+
+*/
 const teigiamas = 5;
 console.log(teigiamas);
+
+const neigiamas = -8;
+const nulis = 0;
+const nulisMinus = -0;
+console.log(nulis, nulisMinus);
+
+const desimtainis = 3.141592564;
+console.log("PY reiksme", desimtainis);
+
+const begelybe = Infinity;
+const begelybeMinus = -Infinity;
+console.log("PVZ", begelybe, begelybeMinus);
+
+const nesamone = NaN;
+console.log(nesamone);
+
+//NaN pavyzdys kodo
+const puseZodzio = "laba" / 2;
+console.log(puseZodzio);
