7236b9bac0ff739f01cec723fd9c56d2873907a5
Virginijus03 <klimauskas@yahoo.com>
2021-05-06 20:29:36 +0300

    swith stoteles

--- js/11-switch.js
+++ js/11-switch.js
@@ -5,3 +5,30 @@ switch (akys) {
     console.log(Melynos);
     break;
 }
+
+/*
+Arbatos ruosimo procesas:
+- paimti puodeli
+- isideti cukraus
+- isideti arbatos
+- uzpilti vandens
+- ismaisyti
+- gerti
+*/
+
+const processStep = 0;
+
+switch (processStep) {
+    case 0:
+        console.log("paimti puodeli");
+        break;
+}
+
+
+const stotele = "Geniu";
+
+switch (stotele) {
+    case "Geniu":
+        console.log("Geniu");
+        break;
+}
