ba4b992210fd68a3009ce7b7e8f5762cbe89fd1a
Virginijus03 <klimauskas@yahoo.com>
2021-05-06 16:43:17 +0300

    NamuDarbai

---
+++ js/07-namudarbai.js
@@ -0,0 +1,78 @@
+//skaiciai
+const v = 14;
+console.log(v);
+
+const k = 03;
+console.log(k);
+
+const l = 83;
+console.log(l);
+
+
+//tekstas
+const vv = "Virginijus";
+console.log(vv);
+
+const kk = "Klimauskas";
+console.log(kk);
+
+const fullNames = vv + ' ' + kk;
+console.log(fullNames);
+
+
+//sarasas skaiciu
+const mt = [19, 13, 54, 78, 77];
+console.log(mt);
+
+const mn = [03, 05, 07, 11, 04];
+console.log(mn);
+
+const dn = [14, 23, 04, 05, 23];
+console.log(dn);
+
+
+//sarasas teksto
+const sarasas1 = ["as", "tu", "jis", "ji","jos"];
+console.log(sarasas1);
+
+const sarasas2 = ["nieko", "niekad", "nesakau", "niekad", ",", "niekados"];
+console.log(sarasas2);
+
+const sarasas3 = ["bandau", "suprasti", "kaip", "reik","kodint"];
+console.log(sarasas3);
+
+
+//veiksmai su kintamaisiais
+const suma = v + k + l;
+console.log(suma); 
+
+/*const suma = mt + mn + dn;
+console.log(suma);*/ 
+
+const sand = 5 * 5;
+console.log(sand); 
+
+const dal = 5 / 5;
+console.log(dal); 
+
+const atim = 5 - 5;
+console.log(atim);
+
+const sarasas4 = "bandau" + " " + "suprasti" + " " + "kaip" + " " + "reik" + " " + "kodint";
+console.log(sarasas4);
+
+const sarasas5 = 14 - 23 + 04 - 05 + 23;
+console.log(sarasas5);
+
+/*Sujungti sąrašų vertes, kurių tipas yra tekstai,
+ nuo sąrašo galo iki pradžios taip, jog tarp jų būtų kablelis ir tarpas.*/
+
+//if - else
+const daugiau = 4 < 2;
+/*const false = daugiau;
+
+if {
+
+}
+*/
+
